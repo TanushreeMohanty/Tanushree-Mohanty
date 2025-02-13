51334827009421a1a5f1f993bff2e4179696efff
TanushreeMohanty <mohantytanushree234@gmail.com>
2025-02-13 14:04:19 +0530

    trial

---
+++ script.js
@@ -0,0 +1,11 @@
+//NAVBAR
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  
